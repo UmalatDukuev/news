cfabf9d6aae8e060429aebe6e0f1d9ed3b2e5f38
Umalat <dukuev037@mail.ru>
2024-11-02 00:11:31 +0300

    1

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"news/internal/handler"
+	"news/internal/repository"
+	"news/internal/service"
 )
 
 func main() {
+	repo := repository.NewRepository()
+	service := service.NewService()
+	handler := handler.NewHandler()
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Server is running...")
 	})
