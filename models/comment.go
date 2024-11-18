bed15603e4600fcc25b47722246d202ddd8c52cf
Umalat <dukuev037@mail.ru>
2024-11-18 22:25:42 +0300

    определены модели

--- models/comment.go
+++ models/comment.go
@@ -1,11 +1,7 @@
-package models
-
-import "time"
-
-type Post struct {
+type Comment struct {
 	ID        int64     `json:"id" db:"id"`
+	PostID    int64     `json:"post_id" db:"post_id"`
 	UserID    int64     `json:"user_id" db:"user_id"`
-	Title     string    `json:"title" db:"title"`
 	Content   string    `json:"content" db:"content"`
 	CreatedAt time.Time `json:"created_at" db:"created_at"`
 }
