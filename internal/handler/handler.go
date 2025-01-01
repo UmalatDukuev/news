566fea8deaa1ec50e1c391990c30802d551baa6b
Umalat <dukuev037@mail.ru>
2025-01-01 03:26:04 +0300

    каркас c функциями авторизации (без реализации)

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -15,5 +15,11 @@ func NewHandler(services *service.Service) *Handler {
 
 func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.New()
+	auth := router.Group("/auth")
+	{
+		auth.POST("sign-up", h.signUp)
+		auth.POST("sign-in", h.signIn)
+
+	}
 	return router
 }
